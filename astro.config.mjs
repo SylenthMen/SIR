d8bc9fc35c60d54d618f8306af39600360442e7c
sylenthmen <manuelene26@gmail.com>
2024-04-06 19:22:01 -0600

    add analitycs of vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astro/vercel/serverless";
 
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapters: vercel({
+    webAnalytics: { enabled: true}
+  }),
   integrations: [tailwind(), svelte()]
 });
